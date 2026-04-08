da08c3e7688ddc1b197a450009dde7ba1e554eb2
jinal-virani <viranijinal7670@gmail.com>
2026-04-08 20:04:48 +0530

    Set some changes

---
+++ src/app/games/allGamesCollection.tsx
@@ -0,0 +1,63 @@
+// "use client";
+
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import { getAllGames } from '@/src/services/gameService';
+import Image from 'next/image';
+
+
+export default async function AllGamesCollection() {
+
+  // 1. Fetch backend data directly (No fetch keshava!)
+  const games = await getAllGames();
+  return (
+    <>
+      <Navbar />
+      <section className="games-hero">
+        <div className="container">
+          <p className="section-tag">Made with love</p>
+          <h1>Games</h1>
+        </div>
+      </section>
+      <section className="games-list">
+        <div className="container">
+          {games.map((game, i) => (
+            <div key={game.title}>
+              <div className={`game-row${i % 2 !== 0 ? ' reverse' : ''}`}>
+                <div className="game-card-image-wrapper-list">
+                  <Image
+                    src={game.thumbnail}
+                    alt={game.title}
+                    fill
+                    className="game-card-img"
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    priority={false}
+                  />
+                </div>
+                <div className="game-info">
+                  <h2>{game.title}</h2>
+                  <p>{game.description}</p>
+                  {/* <p>{game.desc2}</p> */}
+                  <div className="store-badges">
+                    <button className="store-btn">
+                      <a href={game.links?.ios} target="_blank" rel="noopener noreferrer">
+                        <img src="appstore.png" />
+                      </a>
+                    </button>
+                    <button className="store-btn">
+                      <a href={game.links?.android} target="_blank" rel="noopener noreferrer">
+                        <img src="googleplay.png" />
+                      </a>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              {i < games.length - 1 && <div className="game-divider" />}
+            </div>
+          ))}
+        </div>
+      </section>
+      <Footer />
+    </>
+  )
+}
